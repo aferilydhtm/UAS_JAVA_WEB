e4fd98b5bad84e36ef6c1cc54f472201686d4d0d
RendyLorenso <rendylorenso@gmail.com>
2023-12-26 20:25:33 +0700

    aku tambahin configs ya bang

---
+++ src/main/java/com/uas/course/configs/ProjectConfig.java
@@ -0,0 +1,56 @@
+package com.uas.course.configs;
+
+import java.util.Locale;
+import java.util.Optional;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+//import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+@Configuration
+public class ProjectConfig implements WebMvcConfigurer {
+
+	@Bean
+	PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Bean(name = { "auditorAwareImpl" })
+	AuditorAware<String> auditorAwareImpl() {
+		return new AuditorAware<String>() {
+			@Override
+			public Optional<String> getCurrentAuditor() {
+				String email = SecurityContextHolder.getContext().getAuthentication().getName();
+				return Optional.ofNullable(email);
+			}
+		};
+	}
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(localeInterceptor());
+	}
+
+	@Bean
+	LocaleResolver localeResolver() {
+		final SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+		localeResolver.setDefaultLocale(new Locale("en", "US"));
+		return localeResolver;
+	}
+
+	@Bean
+	LocaleChangeInterceptor localeInterceptor() {
+		LocaleChangeInterceptor localeInterceptor = new LocaleChangeInterceptor();
+		localeInterceptor.setParamName("lang");
+		return localeInterceptor;
+	}
+}
