7b153eb8e35b0f45d041da3bbe29077d0a3c2617
aferilydtm <aferilyudhatama04@gmail.com>
2023-12-26 20:17:24 +0700

    add LoggerAspect.java

---
+++ src/main/java/com/uas/course/aspects/LoggerAspect.java
@@ -0,0 +1,33 @@
+package com.uas.course.aspects;
+
+import java.time.Duration;
+import java.time.Instant;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Aspect
+@Component
+@Slf4j
+public class LoggerAspect {
+
+	@Around("execution(* com.uas.course.*.*.*(..))")
+	public Object logTime(ProceedingJoinPoint joinPoint) throws Throwable {
+		log.info(joinPoint.getSignature().toLongString() + "execution starts!");
+		Instant start = Instant.now();
+
+		Object returnObject = joinPoint.proceed();
+
+		Instant end = Instant.now();
+		long timeTaken = Duration.between(start, end).toMillis();
+		log.info("time taken " + timeTaken);
+		log.info(joinPoint.getSignature().toString() + " execution ends!");
+
+		return returnObject;
+	}
+
+}
