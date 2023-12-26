27d55320714e3635bce3011bbbf95920f20ddf07
RendyLorenso <rendylorenso@gmail.com>
2023-12-26 20:11:23 +0700

    nambahin filematch.java

---
+++ src/main/java/com/uas/course/annotations/FieldMatch.java
@@ -0,0 +1,38 @@
+package com.uas.course.annotations;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Repeatable;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import com.uas.course.validations.FieldMatchValidator;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+@Documented
+@Constraint(validatedBy = FieldMatchValidator.class)
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Repeatable(value = FieldMatch.List.class)
+public @interface FieldMatch {
+
+	String message() default "";
+
+	Class<?>[] groups() default {};
+
+	String first();
+
+	String second();
+
+	Class<? extends Payload>[] payload() default {};
+
+	@Documented
+	@Target(ElementType.TYPE)
+	@Retention(RetentionPolicy.RUNTIME)
+	@interface List {
+		FieldMatch[] value();
+	}
+}
